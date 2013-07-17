50d05a3cff9a22cf1807dcff254cdd4f31ccde19
Ashir76 <ashir76@gmail.com>
2013-07-17 13:07:22 +0100

    Added example app

---
+++ ExampleApp/ExampleViewController.m
@@ -0,0 +1,37 @@
+//
+//  ExampleViewController.m
+//  Translator
+//
+//  Created by Jacek Wierzbicki-Woś on 16/07/2013.
+//  Copyright (c) 2013 WEB DESIGN STUDIO Sebastian Owodziń. All rights reserved.
+//
+
+#import "ExampleViewController.h"
+
+#import "WDSTranslator.h"
+#import "WDSTranslator+UIButton.h"
+#import "WDSTranslator+UILabel.h"
+#import "WDSTranslator+UITextField.h"
+
+@interface ExampleViewController ()
+
+@end
+
+@implementation ExampleViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+        
+	// Do any additional setup after loading the view.
+    
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ExampleApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExampleApp
+//
+//  Created by Jacek Wierzbicki-Woś on 16/07/2013.
+//  Copyright (c) 2013 WEB DESIGN STUDIO Sebastian Owodziń. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
